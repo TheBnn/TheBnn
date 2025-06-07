3d59e50b2fd97b151e2655d4eb485d36667556a6
Konstantin Melnikov <37793494+TheBnn@users.noreply.github.com>
2025-06-07 21:10:41 +0300

    Update data package

--- app/src/main/java/com/example/bagstash/data/CycleDao.kt
+++ app/src/main/java/com/example/bagstash/data/CycleDao.kt
@@ -1,4 +1,4 @@
-package com.example.packagingapp.data
+package com.example.bagstash.data
 
 import androidx.room.Dao
 import androidx.room.Insert
--- app/src/main/java/com/example/bagstash/data/Department.kt
+++ app/src/main/java/com/example/bagstash/data/Department.kt
@@ -1,4 +1,4 @@
-package com.example.packagingapp.data
+package com.example.bagstash.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
